b616155d09e2a41e80d3ee855b93f865b8661751
DmitryTyurin <dwtyurin@gmail.com>
2024-09-08 00:42:22 +0300

    3.9 Комбинация различных параметров

--- python/Functional_Programming_in_Python/module_3_function_parameters_and_arguments/3.9.py
+++ python/Functional_Programming_in_Python/module_3_function_parameters_and_arguments/3.9.py
@@ -5,17 +5,15 @@
 # high={max}, low={min}, avg={average}
 # где max - принимает значение самого большого аргумента, min - принимает значение самого маленького аргумента, average - принимает среднее арифметическое самого большого и самого маленького из аргументов.
 # Напомню что return сразу же производит выход из функции.
+
+
 def calculate_high_low_avg(*args, log_to_console=False):
     if args:
         max_value = max(args)
         min_value = min(args)
         average = (max_value + min_value) / 2
         if log_to_console:
-            print(f'high={max_value}, low={min_value}, avg={average}')
+            print(f"high={max_value}, low={min_value}, avg={average}")
         return average
     else:
         return None
-
-
-
-
