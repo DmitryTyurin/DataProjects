ab6db3cb2e959a136bbf0871739f2fee8f4c62c4
DmitryTyurin <dwtyurin@gmail.com>
2024-10-04 19:13:40 +0300

    Продвинутые фичи SQL. Практические задачи

--- sql/sql_workshop_advanced_level/2.11.sql
+++ sql/sql_workshop_advanced_level/2.11.sql
@@ -18,3 +18,30 @@ END;
 
 SELECT deactivate_infants();
 
+
+Нашу функцию deactivate_infants необходимо доработать. А именно: деактивировать несовершеннолетние аккаунты только в случае, если они бесплатные.
+Код должен содержать несколько команд.
+Предварительно удалите существующую функцию с проверкой
+Деактивируйте учетные записи только если возраст < 18 и is_premium_account = false
+Для получения нужных строк используйте конструкцию UPDATE-JOIN
+Функция должна вернуть количество записей, которые она обновила (системная функция ROW_COUNT() в помощь)
+
+DROP FUNCTION IF EXISTS deactivate_infants;
+
+CREATE FUNCTION deactivate_infants() RETURNS INT
+MODIFIES SQL DATA
+BEGIN
+    DECLARE result INT DEFAULT 0;
+
+    UPDATE users u
+    INNER JOIN user_settings us ON u.id = us.user_id
+    SET u.is_active = 0
+    WHERE us.is_premium_account = false AND (YEAR(NOW()) - YEAR(u.birthday)) < 18;
+
+    SELECT COUNT(*) INTO result FROM users u
+    WHERE u.is_active = 0;
+
+    SET result = ROW_COUNT();
+
+    RETURN result;
+END;
