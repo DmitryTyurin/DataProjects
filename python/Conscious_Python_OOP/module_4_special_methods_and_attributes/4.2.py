03a94beca9a70fa90ab6a48f59b4bd7f3a75cf24
DmitryTyurin <dwtyurin@gmail.com>
2024-10-16 18:43:19 +0300

    4.2 Методы для атрибутов

--- python/Conscious_Python_OOP/module_4_special_methods_and_attributes/4.2.py
+++ python/Conscious_Python_OOP/module_4_special_methods_and_attributes/4.2.py
@@ -53,3 +53,37 @@ class OnlyVasya:
 obj = OnlyVasya()
 obj.name = "Masha"
 print(obj.name)
+
+
+# Создайте класс Number и объявите в нём два метода (__init__, __delattr__).
+# Метод __init__ :
+# - содержит параметры (self, a, b)
+# - создаёт атрибуты "a", "b" с соответствующими значениями a, b
+# - создаёт атрибут "s" который равен сумме "a + b"
+# - удаляет атрибут "s"
+# Метод __delattr__ :
+# - содержит параметры (self, name)
+# - содержит условие, если атрибут "s" больше 10, то ничего не делаем (pass). Иначе производим стандартное удаление с помощью super().__delattr__(name)
+# Код проверки не удаляйте, пожалуйста
+
+
+class Number:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+        self.s = a + b
+        del self.s
+
+    def __delattr__(self, name):
+        if self.s > 10:
+            pass
+        else:
+            super().__delattr__(name)
+
+
+# код проверки:
+number1 = Number(4, 5)
+print("s" in number1.__dict__)
+
+number2 = Number(6, 11)
+print("s" in number2.__dict__)
