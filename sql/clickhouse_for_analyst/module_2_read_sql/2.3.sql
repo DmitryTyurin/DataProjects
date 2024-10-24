3e82262ac00cfacd2f0ea8b9c765d13fbe85ed22
DmitryTyurin <dwtyurin@gmail.com>
2024-10-24 18:23:11 +0300

    2.3 Типы данных и группировка

--- sql/clickhouse_for_analyst/module_2_read_sql/2.3.sql
+++ sql/clickhouse_for_analyst/module_2_read_sql/2.3.sql
@@ -15,6 +15,35 @@ order by dt desc
 limit 1;
 
 
+--Мы с вами уже изучали функцию SUM для группировок, однако такой же функции для произведения не существует,
+--вам необходимо написать скрипт который будет перемножать числа в столбце.
+--Можно использовать только математические функции.
+--Вам нужно вспомнить про численный тип данных и школьную математику, а точнее логарифмы.
+
+select floor(exp(sum(log(PassengerId)))) AS product
+from titanic
+where PassengerId < 10;
+
+
+--Посчитайте сумму продаж по Global_Sales по всем издателям, кроме топ 5 по продажам за все время .
+--Задачу можно решить с помощью not in (select ... from ...) для того чтобы исключить лишних издателей.
+--Для начала вам нужно найти всех издателей которые входят в топ 5 по продажам за все время, после чего исключить их из выборки и посчитать сумму по Global_Sales.
+
+with top_5_publisher as
+(
+    select Publisher, sum(Global_Sales) as sum_global_sales
+    from video_game_sales vgs
+    group by Publisher
+    order by sum_global_sales desc
+limit 5
+)
+
+select floor(sum(Global_Sales)) as sum_global_sales_without_top_5
+from video_game_sales vgs
+where Publisher not in (select Publisher from top_5_publisher)
+order by sum_global_sales_without_top_5 desc;
+
+
 --Перечислите через запятую в порядке убывания: в какие годы топ 5 издателей по продажам за все время превышали продажи
 --Global_Sales всех остальных более чем на 60 Ссылка на описание датасета.
 --Подсказка: в комбинатор функций можно вставлять подзапросы sumIf( Global_Sales, publisher not in (SELECT ...))
