c75d9333f83829047b5a93f1c8f0ac5977015ff8
DmitryTyurin <dwtyurin@gmail.com>
2024-10-24 18:19:07 +0300

    2.3 Типы данных и группировка

--- sql/clickhouse_for_analyst/module_2_read_sql/2.3.sql
+++ sql/clickhouse_for_analyst/module_2_read_sql/2.3.sql
@@ -14,3 +14,32 @@ from earnings
 order by dt desc
 limit 1;
 
+
+--Перечислите через запятую в порядке убывания: в какие годы топ 5 издателей по продажам за все время превышали продажи
+--Global_Sales всех остальных более чем на 60 Ссылка на описание датасета.
+--Подсказка: в комбинатор функций можно вставлять подзапросы sumIf( Global_Sales, publisher not in (SELECT ...))
+--План запроса
+--Первый столбец - год
+--Второй - продажи топ 5 издателей
+--Третий - продажи кроме топ 5 издателей
+--Четвертый - посчитайте разницу между 2 и 3
+--Отсортируйте результаты по годам и оставьте только те что удовлетворяют условию >60
+--Годы через запятую в порядке убывания.
+
+with top_5_publisher as
+(
+	select Publisher, sum(Global_Sales) as sum_global_sales
+	from video_game_sales vgs
+	group by Publisher
+	order by sum_global_sales desc
+	limit 5
+)
+select Year,
+	sum(Global_Sales) as all_sales,
+	sumIf(Global_Sales, Publisher in (select Publisher from top_5_publisher)) as top5,
+	sumIf(Global_Sales, Publisher not in (select Publisher from top_5_publisher)) as not_top5,
+	top5 - not_top5 as delta
+from default.video_game_sales
+group by Year
+having delta > 60
+order by Year desc;
