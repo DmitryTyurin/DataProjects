de18df41c80437b68a649c67cfbb9c4adeec3fc9
DmitryTyurin <dwtyurin@gmail.com>
2024-11-01 18:04:41 +0300

    3.1 Базовые метрики. Часть 1

--- sql/clickhouse_for_analyst/module_3_analytics/3.1.sql
+++ sql/clickhouse_for_analyst/module_3_analytics/3.1.sql
@@ -7,3 +7,24 @@ group by month
 having month = '2023-02-01'
 
 
+--Sticky Factor (Липкость) - отношение DAU/MAU то как часто пользователи заходят в приложение в течение месяца.
+--Посчитайте данный показатель для month = '2023-01-01' возьмите avg от этого отношения
+
+with
+(
+	select uniqExact(uid)
+	from login
+	where toStartOfMonth(event_time) = '2023-01-01'
+) as MAU,
+daily_data as (
+	select
+        toStartOfDay(event_time) as day,
+        uniqExact(uid) as DAU,
+		MAU
+     from login
+     where toStartOfMonth(event_time) = '2023-01-01'
+     group by day
+)
+select
+    round(avg(DAU / MAU), 4) as sticky_factor
+from daily_data;
