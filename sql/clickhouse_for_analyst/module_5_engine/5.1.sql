315e16204f3559232e1c760fa7841b12ff04db55
DmitryTyurin <dwtyurin@gmail.com>
2024-11-10 11:29:17 +0300

    5.1 MergeTree Family

--- sql/clickhouse_for_analyst/module_5_engine/5.1.sql
+++ sql/clickhouse_for_analyst/module_5_engine/5.1.sql
@@ -7,13 +7,13 @@
 -- drop table if exists sandbox.sales_dtyurin
 create table if not exists sandbox.sales_dtyurin
 (
-    sale_date 				DateTime			comment 'дата продажи',
-    product_id 				Int64 				comment 'идентификатор продукта',
-    product_category 		String 				comment 'категория продукта',
-    sale_amount 			Int64	default 0 	comment 'сумма продажи',
-    sale_quantity 			Int64 				comment 'количество продаж',
-    customer_id 			Int64	 			comment	'идентификатор покупателя',
-    store_id				Int64 				comment 'идентификатор магазина'
+    sale_date 				DateTime			                    comment 'дата продажи',
+    product_id 				Int64 				                    comment 'идентификатор продукта',
+    product_category 		String 				                    comment 'категория продукта',
+    sale_amount 			Int64	default 0 	                    comment 'сумма продажи',
+    sale_quantity 			Int64 				                    comment 'количество продаж',
+    customer_id 			Int64	 			                    comment	'идентификатор покупателя',
+    store_id				Int64 				                    comment 'идентификатор магазина'
 )
 engine = MergeTree
 partition by toYYYYMM(sale_date)
@@ -24,13 +24,13 @@ order by (sale_date, product_id, customer_id)
 -- drop table if exists sandbox.sales_5_1_2
 create table if not exists sandbox.sales_5_1_2
 (
-    sale_date 				DateTime			comment 'дата продажи',
-    product_id 				Int64 				comment 'идентификатор продукта',
-    product_category 		String 				comment 'категория продукта',
-    sale_amount 			Int64	default 0 	comment 'сумма продажи',
-    sale_quantity 			Int64 				comment 'количество продаж',
-    customer_id 			Int64	 			comment	'идентификатор покупателя',
-    store_id				Int64 				comment 'идентификатор магазина'
+    sale_date 				DateTime			                    comment 'дата продажи',
+    product_id 				Int64 				                    comment 'идентификатор продукта',
+    product_category 		String 				                    comment 'категория продукта',
+    sale_amount 			Int64	default 0 	                    comment 'сумма продажи',
+    sale_quantity 			Int64 				                    comment 'количество продаж',
+    customer_id 			Int64	 			                    comment	'идентификатор покупателя',
+    store_id				Int64 				                    comment 'идентификатор магазина'
 )
 engine = ReplacingMergeTree
 partition by toYYYYMM(sale_date)
@@ -64,12 +64,12 @@ order by (product_category)
 --drop table if exists sandbox.user_active__mv;
 create materialized view if not exists sandbox.user_active__mv
 (
-	id 				UInt32 				comment 'id пользователя',
-	name 			String 				comment 'имя пользователя',
-	value 			Float64 			comment 'значение',
-	is_active 		Bool 				comment 'активный пользователь',
-	key 			Int64 				comment 'ключ',
-	list 			Array(Int64) 		comment 'список'
+	id 				UInt32 				                            comment 'id пользователя',
+	name 			String 				                            comment 'имя пользователя',
+	value 			Float64 			                            comment 'значение',
+	is_active 		Bool 				                            comment 'активный пользователь',
+	key 			Int64 				                            comment 'ключ',
+	list 			Array(Int64) 		                            comment 'список'
 )
 engine = MergeTree
 order by (tuple())
