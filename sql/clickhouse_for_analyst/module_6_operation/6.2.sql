bc61651dfec8077ae3eefdf8f378fe088f1a834b
DmitryTyurin <dwtyurin@gmail.com>
2024-11-14 10:36:41 +0300

    6.2 Эксплуатация

--- sql/clickhouse_for_analyst/module_6_operation/6.2.sql
+++ sql/clickhouse_for_analyst/module_6_operation/6.2.sql
@@ -5,3 +5,16 @@
 select round(sum(bytes_on_disk) / 1024) as size_in_kb
 from system.parts
 where table = 'log_mt'
+
+
+--В данном задании вам необходимо написать запрос к системным таблицам который:
+--выведет для таблицы log_mt запрос select с самым большим количеством использования памяти memory_usage за дату 2024-09-08
+
+select memory_usage
+from system.query_log
+where 1
+	and query_kind = 'Select'
+	and has(tables, 'default.log_mt')
+	and toDate(event_date) = '2024-09-08'
+order by memory_usage desc
+limit 1;
