6864699495eb57a6a0db5512baa8a2753e0ebce5
Dmitrii Tiurin <dtiurin@neuro.net>
2024-12-14 23:31:47 +0300

    2.1 Установка Selenium и WebDriver

--- python/Selenium_Python/module_2/2.1.py
+++ python/Selenium_Python/module_2/2.1.py
@@ -4,6 +4,8 @@ from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.core.os_manager import ChromeType
 from selenium.webdriver.chrome.service import Service as ChromiumService
 
-with webdriver.Chrome(service=ChromiumService(ChromeDriverManager().install())) as driver:
+with webdriver.Chrome(
+    service=ChromiumService(ChromeDriverManager().install())
+) as driver:
     driver.get("https://stepik.org/course/104774")
     time.sleep(5)
