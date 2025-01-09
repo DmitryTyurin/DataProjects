3b6c065678c2658475c18a2fe92b34450e027c98
DmitryTyurin <dwtyurin@gmail.com>
2025-01-09 12:34:30 +0300

    Шестизначный код

---
+++ python/Regular_Expressions_in_Python/module_2/2.6.15.py
@@ -0,0 +1,25 @@
+# Напишите регулярное выражение, которое находит все шестизначные коды подтверждения.
+
+import re
+
+regex = r"\d{6}"
+
+
+class RegularExpression:
+    """
+    Класс для работы с регулярными выражениями
+    """
+
+    def __init__(self, regular: str, text: str):
+        self.regular = regular
+        self.text = text
+
+    def get_result(self) -> str:
+        result = re.findall(self.regular, self.text)
+        result = " ".join(result)
+
+        return result
+
+
+r = RegularExpression(regex, input())
+print(r.get_result())
