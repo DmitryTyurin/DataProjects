e77afb8ae5b1ae8a2b0ad5b4e2b1150c7eee3f11
DmitryTyurin <dwtyurin@gmail.com>
2025-01-09 15:09:31 +0300

    Строгий режим

---
+++ python/Regular_Expressions_in_Python/module_2/2.8.6.py
@@ -0,0 +1,25 @@
+# Напишите регулярное выражение, которое найдёт все use strict; и use strict в тексте.
+
+import re
+
+regex = r"use\sstrict;?"
+
+
+class RegularExpression:
+    """
+    Класс для работы с регулярными выражениями
+    """
+
+    def __init__(self, regular: str, text: str):
+        self.regular = regular
+        self.text = text
+
+    def get_result(self) -> str:
+        result = re.findall(self.regular, self.text)
+        result = " ".join(result)
+
+        return result
+
+
+r = RegularExpression(regex, input())
+print(r.get_result())
