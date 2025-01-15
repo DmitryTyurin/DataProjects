8fab8840c3268f07a76ce3150e29874f69e76149
DmitryTyurin <dwtyurin@gmail.com>
2025-01-15 12:57:10 +0300

    Полёт на Марс

---
+++ python/Regular_Expressions_in_Python/module_3/3.3.3.py
@@ -0,0 +1,33 @@
+# Найти строку, в которой есть последовательности код или Код. Получить номер этой строки и номер начала вхождения последовательности.
+# На вход программе подаются 4 строки.
+# Выведите в консоль номер строки, в которой было найдено вхождение, и через пробел начальную позицию этого вхождения, если они были найдены.
+# Иначе выведите строку: код не найден
+
+import re
+import sys
+
+text = list(map(str.strip, sys.stdin.readlines()))
+pattern = re.compile(r'код', re.IGNORECASE)
+
+class RegularExpression:
+    """
+    Класс для работы с регулярными выражениями
+    """
+
+    def __init__(self, text: str, pattern):
+        self.text = text
+        self.pattern = pattern
+
+    def get_result(self):
+        for i, t in enumerate(self.text, start=1):
+            match = self.pattern.search(t)
+            if match:
+                print(f"{i} {match.start()}")
+                break
+        else:
+            print("код не найден")
+
+
+
+r = RegularExpression(text, pattern)
+r.get_result()
