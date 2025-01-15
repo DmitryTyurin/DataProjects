30a86d9dfd9d0f4320c22b5c7f0560730c7e7aca
DmitryTyurin <dwtyurin@gmail.com>
2025-01-15 13:19:33 +0300

    Полёт на Марс

--- python/Regular_Expressions_in_Python/module_3/3.3.3.py
+++ python/Regular_Expressions_in_Python/module_3/3.3.3.py
@@ -7,7 +7,8 @@ import re
 import sys
 
 text = list(map(str.strip, sys.stdin.readlines()))
-pattern = re.compile(r'код', re.IGNORECASE)
+pattern = re.compile(r"код", re.IGNORECASE)
+
 
 class RegularExpression:
     """
@@ -26,7 +27,6 @@ class RegularExpression:
                 break
         else:
             print("код не найден")
-
 
 
 r = RegularExpression(text, pattern)
