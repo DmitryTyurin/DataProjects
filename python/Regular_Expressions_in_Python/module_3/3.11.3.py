75820f22bd7253ee5b415497462ca3377bb6c512
DmitryTyurin <dwtyurin@gmail.com>
2025-01-17 12:37:53 +0300

    Ссылка

---
+++ python/Regular_Expressions_in_Python/module_3/3.11.3.py
@@ -0,0 +1,21 @@
+import re
+import sys
+
+string = rf"{input()}"
+
+class RegularExpression:
+    """
+    Класс для работы с регулярными выражениями
+    """
+
+    def __init__(self, string: str):
+        self.string = string
+
+    def get_result(self):
+        result = re.escape(self.string)
+
+        return result
+
+
+r = RegularExpression(string)
+print(r.get_result())
