0882b4371ecb3ea0c2ab603da1e894aae8c2cfff
DmitryTyurin <dwtyurin@gmail.com>
2025-01-17 11:05:28 +0300

    Гласные запрещены

---
+++ python/Regular_Expressions_in_Python/module_3/3.9.2.py
@@ -0,0 +1,26 @@
+# Замените все гласные на восклицательный знак.
+
+import re
+import sys
+
+pattern = r"[aeioyuауоыиэяюёе]"
+string = input()
+
+
+class RegularExpression:
+    """
+    Класс для работы с регулярными выражениями
+    """
+
+    def __init__(self, pattern: str, string: str):
+        self.string = string
+        self.pattern = pattern
+
+    def get_result(self):
+        result = re.sub(self.pattern, "!",self.string, flags=re.IGNORECASE)
+
+        return result
+
+
+r = RegularExpression(pattern, string)
+print(r.get_result())
