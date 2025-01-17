4a62dd6abf2a0dbbaaeb839ab0c5c483eb5278d1
DmitryTyurin <dwtyurin@gmail.com>
2025-01-17 12:50:38 +0300

    Бюрократия

--- python/Regular_Expressions_in_Python/module_3/3.9.4.py
+++ python/Regular_Expressions_in_Python/module_3/3.9.4.py
@@ -8,11 +8,12 @@ import re
 import sys
 
 fio = input().split()
-f1, i1, o1 = fio[0], fio[1][0]+'.', fio[2][0]+'.'
+f1, i1, o1 = fio[0], fio[1][0] + ".", fio[2][0] + "."
 f2, i2, o2 = fio[0], fio[1][:-1], fio[2]
 
-pattern = fr'({f1}[а-я]*?[ ]{i1}[ ]{o1})|({f2}[а-я]*?[ ]{i2}[а-я]*?[ ]{o2}[а-я]*?)\b'
+pattern = rf"({f1}[а-я]*?[ ]{i1}[ ]{o1})|({f2}[а-я]*?[ ]{i2}[а-я]*?[ ]{o2}[а-я]*?)\b"
 string = input()
+
 
 class RegularExpression:
     """
@@ -24,7 +25,7 @@ class RegularExpression:
         self.pattern = pattern
 
     def get_result(self):
-        result = re.sub(self.pattern, "ФИО",self.string, flags=re.IGNORECASE)
+        result = re.sub(self.pattern, "ФИО", self.string, flags=re.IGNORECASE)
 
         return result
 
