138d9ce8fca0acdd5e90b17a1e00a26da9bc1d57
DmitryTyurin <dwtyurin@gmail.com>
2025-01-30 11:21:11 +0300

    Почините await #3

---
+++ python/Asynchronous_Python/module_3_awaitable_объекты/3.6.14.py
@@ -0,0 +1,24 @@
+# В коде пропущено ключевое слово await, которое необходимо для ожидания асинхронных операций.
+# Вам нужно найти место, где его отсутствие мешает правильной работе программы.
+
+
+import asyncio
+
+
+async def compute_square(x):
+    print(f"Вычисляем квадрат числа: {x}")
+    await asyncio.sleep(1)  # Имитация длительной операции
+    return x * x
+
+
+async def main():
+    # Создаём и запускаем задачи
+    tasks = [asyncio.create_task(compute_square(i)) for i in range(10)]
+    # Ожидаем завершения всех задач и собираем результаты
+    results = await asyncio.gather(*tasks)
+
+    for result in results:
+        print(f"Результат: {result}")
+
+
+asyncio.run(main())
