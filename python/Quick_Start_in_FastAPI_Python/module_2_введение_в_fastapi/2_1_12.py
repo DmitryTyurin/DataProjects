e32489f0ec7142c2df73c3810af377a76f873037
Dmitrii Tiurin <dtiurin@neuro.net>
2025-03-18 22:48:11 +0300

    Задание по программированию

--- python/Quick_Start_in_FastAPI_Python/module_2_введение_в_fastapi/2_1_12.py
+++ python/Quick_Start_in_FastAPI_Python/module_2_введение_в_fastapi/2_1_12.py
@@ -1,14 +1,8 @@
 from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
+from fastapi.responses import FileResponse
 
 app = FastAPI()
 
-# Загружаем содержимое HTML-файла
-with open("index.html", "r", encoding="utf-8") as file:
-    html_content = file.read()
-
-
-# Возвращаем HTML-страницу при GET-запросе к корневому маршруту
-@app.get("/", response_class=HTMLResponse)
-async def read_root():
-    return html_content
+@app.get("/")
+async def root():
+    return FileResponse('index.html')
