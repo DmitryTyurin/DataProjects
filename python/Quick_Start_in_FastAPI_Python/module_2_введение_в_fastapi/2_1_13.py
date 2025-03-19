946945eb613cc0df903e2634d336241d4c01b7ac
Dmitrii Tiurin <dtiurin@neuro.net>
2025-03-19 22:15:19 +0300

    Задание по программированию повышенной сложности

---
+++ python/Quick_Start_in_FastAPI_Python/module_2_введение_в_fastapi/2_1_13.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Numbers(BaseModel):
+    num1: float
+    num2: float
+
+
+@app.post("/calculate")
+async def calculate(numbers: Numbers):
+    result = numbers.num1 + numbers.num2
+
+    return {"result": result}
