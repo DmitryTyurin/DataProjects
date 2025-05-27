28fd6abe4ee41393fc90fab7d0a373435efa8f7a
DmitryTyurin <dwtyurin@gmail.com>
2025-05-27 22:44:34 +0300

    Загрузка видео с помощью библиотеки requests

---
+++ python/WEB_Parsing_Python/module_3_requests/3.2.19.py
@@ -0,0 +1,20 @@
+# Перейдите на сайт
+# Скачайте видео с сайта  при помощи requests
+# Определите его размер вручную
+# Напишите размер файла в поле для ответа. Написать нужно только число в мегабайтах.
+
+import requests
+
+url = "https://parsinger.ru/video_downloads/videoplayback.mp4"
+
+try:
+    response = requests.get(url, stream=True)
+    response.raise_for_status()
+
+    file_size_bytes = int(response.headers.get('content-length', 0))
+    file_size_mb = round(file_size_bytes / (1024 * 1024), 2)
+
+    print(file_size_mb)
+
+except requests.exceptions.RequestException as e:
+    print(f"Ошибка при скачивании файла: {e}")
