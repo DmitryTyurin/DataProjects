da6726403eef436662a8d195b13ed37644e3ee3b
DmitryTyurin <dwtyurin@gmail.com>
2025-06-09 16:55:21 +0300

    🔢Сумма значений первого столбца

---
+++ python/WEB_Parsing_Python/module_4_beautifulsoup/4.9.5.py
@@ -0,0 +1,57 @@
+# Перейдите на сайт и найдите целевую таблицу.
+# Выполните парсинг данных из первого столбца таблицы.
+# Извлеките все числа из ячеек первого столбца, исключая числа из заголовка.
+# Рассчитайте сумму извлечённых чисел.
+# Вставьте результат в поле ответа Степик в формате числа с плавающей точкой(3 символа после запятой).
+
+from bs4 import BeautifulSoup
+import requests
+from concurrent.futures import ThreadPoolExecutor
+
+
+class DataSoup:
+    def __init__(self):
+        self.session = requests.Session()
+        self.url = "https://parsinger.ru/table/2/index.html"
+        self.results = []
+
+    def get_html(self):
+        try:
+            response = self.session.get(self.url)
+            response.encoding = "utf-8"
+            response.raise_for_status()
+
+            return response.text
+
+        except requests.RequestException as e:
+            print(f"Ошибка при запросе {self.url}: {e}")
+            return None
+
+    def unique_numbers(self, df):
+
+        unique_values = df.loc[0].drop_duplicates().tolist()
+
+        self.results.extend(unique_values)
+
+    def get_data(self):
+        import pandas as pd
+        from io import StringIO
+
+        data = self.get_html()
+
+        df = pd.read_html(StringIO(data))[0]
+        self.unique_numbers(df)
+
+    def sum(self):
+        set_results = set(self.results)
+
+        return sum(set_results)
+
+
+def main():
+    ds = DataSoup()
+    ds.get_data()
+    print(ds.sum())
+
+
+main()
