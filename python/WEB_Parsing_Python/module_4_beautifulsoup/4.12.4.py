4e7a6fa1ce459b6724b8691ff478c7e6083ade02
DmitryTyurin <dwtyurin@gmail.com>
2025-06-10 16:37:43 +0300

    Вычисление стоимости товаров в каждой категории

---
+++ python/WEB_Parsing_Python/module_4_beautifulsoup/4.12.4.py
@@ -0,0 +1,76 @@
+# Вашей задачей является обработка данных в формате JSON, полученных по ссылке ​. Для подсчета общего количества товаров в разных категориях. Каждая карточка товара содержит информацию о количестве данного товара.
+#
+# Ожидаемый вывод:
+# На выходе вашей программы должен быть словарь в одну строку в формате Python:
+#
+# {'watch': N, 'mobile': N, 'mouse': N, 'hdd': N, 'headphones': N}
+# где N — это общее количество товаров для каждой категории.
+
+from bs4 import BeautifulSoup
+import requests
+from concurrent.futures import ThreadPoolExecutor
+
+
+class DataSoup:
+    def __init__(self):
+        self.session = requests.Session()
+        self.url = "https://parsinger.ru/downloads/get_json/res.json"
+        self.categories_count = {}
+
+    def get_html(self):
+        with self.session.get(self.url) as response:
+            try:
+                response.encoding = "utf-8"
+                response.raise_for_status()
+
+                return response.json()
+
+            except requests.RequestException as e:
+                print(f"Ошибка при запросе {self.url}: {e}")
+                return None
+
+    @staticmethod
+    def extract_digits(text):
+        import re
+
+        pattern = r"\d{1,}\.\d{1,}|\d{1,}"
+        digits = "".join(re.findall(pattern, text))
+
+        return int(digits)
+
+    def get_data(self):
+        import json
+
+        data = self.get_html()
+
+        for item in data:
+            categories = item.get("categories")
+            count = int(item.get("count", 0))
+            price = int(self.extract_digits(item.get("price", 0)))
+
+            if categories in self.categories_count:
+                self.categories_count[categories] += count * price
+            else:
+                self.categories_count[categories] = count * price
+
+        if self.categories_count:
+            return json.dumps(self.categories_count, ensure_ascii=False)
+        else:
+            return "Нет данных для обработки"
+
+
+def main():
+    import time
+
+    start = time.perf_counter()
+
+    ds = DataSoup()
+
+    print(ds.get_data())
+
+    end = time.perf_counter()
+
+    print(f"Время выполнения: {end - start} секунд")
+
+
+main()
