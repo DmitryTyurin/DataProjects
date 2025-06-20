7b2e694bd101ebabe447ce8270b644e8230558b4
DmitryTyurin <dwtyurin@gmail.com>
2025-06-20 11:31:45 +0300

    Применяем context_click() с кастомным контекстным меню

---
+++ python/WEB_Parsing_Python/module_5_selenium/5.8.13.py
@@ -0,0 +1,64 @@
+# 1️⃣ Перейдите на сайт-тренажёр с помощью Selenium.
+# 2️⃣ Используя метод context_click(), выполните правый клик по элементу с id="context-area", чтобы отобразилось кастомное контекстное меню.
+# 3️⃣ В появившемся меню, используя поиск по CSS-селектору, найдите пункт с атрибутом data-action="get_password" и кликните по нему.
+# 4️⃣ Извлеките пароль из появившегося элемента с атрибутом key="access_code" и вставьте его в поле ниже между кавычками.
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+
+
+class DataDriver:
+    def __init__(self):
+        self.url = "https://parsinger.ru/selenium/7/7.3.4/index.html"
+        self.options = self.setup_options()
+        self.driver = webdriver.Chrome(options=self.options)
+        self.action = ActionChains(self.driver)
+
+    @staticmethod
+    def setup_options():
+        from selenium.webdriver.chrome.options import Options
+
+        options = Options()
+        # options.add_argument("--headless")  # Без графического интерфейса
+        # options.add_argument('--disable-gpu')  # Отключаем GPU
+        options.add_argument("--window-size=1920,1080")  # Устанавливаем размер окна
+
+        return options
+
+    def get_result(self, driver, url):
+        import time
+
+        driver.get(url)
+
+        context_area = driver.find_element(By.ID, "context-area")
+        self.action.context_click(context_area).perform()
+        driver.find_element(By.CSS_SELECTOR, '[data-action="get_password"]').click()
+
+        result = driver.find_element(By.CSS_SELECTOR, '[key="access_code"]').text
+        result = result.split(":")[-1].strip()
+
+        return result
+
+    def run(self):
+        with self.driver as driver:
+            data = self.get_result(driver, self.url)
+
+            print(data)
+
+
+def main():
+    import time
+
+    start_time = time.perf_counter()
+
+    d = DataDriver()
+    d.run()
+
+    end_time = time.perf_counter()
+
+    print(f"Время выполнения: {end_time - start_time:.2f} секунд")
+
+
+main()
