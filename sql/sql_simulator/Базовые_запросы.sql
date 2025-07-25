926920c21337fd5b5177bf7c8018327b10297ca6
DmitryTyurin <dwtyurin@gmail.com>
2025-07-25 23:45:19 +0300

    Базовые запросы

--- sql/sql_simulator/Базовые_запросы.sql
+++ sql/sql_simulator/Базовые_запросы.sql
@@ -88,3 +88,14 @@ SELECT concat('Заказ № ', order_id, ' создан ', creation_time::date
 FROM   orders limit 200
 
 
+-- Выведите id всех курьеров и их годы рождения из таблицы couriers.
+-- Год рождения необходимо получить из колонки birth_date. Новую колонку с годом назовите birth_year. Результат отсортируйте сначала по убыванию года рождения курьера (т.е. от самых младших к самым старшим), затем по возрастанию id курьера.
+-- Поля в результирующей таблице: courier_id, birth_year
+
+select
+    courier_id,
+    date_part('year', birth_date) as birth_year
+from couriers
+order by birth_year desc, courier_id asc
+
+
